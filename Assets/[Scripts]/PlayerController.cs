9beeb67e4422d6e5f4246bce20ff171efe5a4c8b
woonhak-kong <woonhak.kong@georgebrown.ca>
2022-02-18 18:16:55 -0500

    Movement is implemented by new Input system

--- Assets/[Scripts]/PlayerController.cs
+++ Assets/[Scripts]/PlayerController.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 4.0f;
+    public float jumpForce = 400.0f;
+
+    private Rigidbody2D _rigidbody2D;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody2D =  GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -15,4 +23,26 @@ public class PlayerController : MonoBehaviour
     {
         
     }
+
+    private void OnMove(InputValue input)
+    {
+        
+        Vector2 velocity = input.Get<Vector2>() * speed;
+        //_rigidbody2D.AddForce(velocity, ForceMode2D.Impulse);
+        Debug.Log("Input - " + velocity);
+        _rigidbody2D.velocity = velocity;
+    }
+
+
+    private void OnJump()
+    {
+        Debug.Log("OnJump");
+        _rigidbody2D.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void OnTeleport(InputValue input)
+    {
+        Debug.Log("OnTeleport");
+    }
+
 }
